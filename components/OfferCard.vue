417660d1ba786b314c1241fbf8b039f6f34b91c6
coderemon24 <mdemonahmed2021@gmail.com>
2025-08-13 18:44:23 +0600

    support and offers card designed

---
+++ components/OfferCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="
+  p-7
+  rounded-lg
+  flex
+  flex-col
+  items-center
+  gap-2
+  text-black/90
+  group
+  cursor-pointer
+  ">
+     <span class="
+     text-3xl
+     bg-blue-700/20
+     w-16
+     h-16
+     flex
+     items-center
+     justify-center
+     rounded-full
+     group-hover:bg-blue-800
+     group-hover:text-white
+     transition-all
+     ease-in-out
+     duration-300
+     ">
+        <i :class="icon"></i>
+     </span>
+     <div class="">
+        <h3 class="
+        text-center
+        text-xl
+        font-semibold
+        leading-12
+        ">{{ title }}</h3>
+        <p class="
+        text-center
+        text-gray-600
+        ">
+            {{ description }}
+        </p>
+     </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  icon: String,
+  title: String,
+  description: String,
+})
+
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,60 @@
 <template>
    <div>
     <!-- hero section-->
-     <section class="
-     mb-4
-     ">
+     <section>
           <AppSwiper />
      </section>
  
+     <!-- what we offer -->
+      <section class="
+      py-12
+      bg-gray-100
+      ">
+        <div class="
+        container mx-auto w-11/12
+        ">
+          <div class="
+          grid
+          grid-cols-1
+          md:grid-cols-3
+          gap-5
+          ">
+            <OfferCard
+            v-for="item in cardItems"
+            :icon="item.icon" 
+            :title="item.title"
+            :description="item.description"
+            class="
+            bg-white
+            shadow-xl
+            " />
+          </div>
+        </div>
+      </section>
+     
    </div>
  </template>
  
  <script lang="ts" setup>
+import { OfferCard } from '#components';
 
+const cardItems = ref([
+    {
+        icon: 'fa-solid fa-graduation-cap',
+        title: 'Up To 100% Scholarship',
+        description: 'You might be eligible for a scholarship, grant, or funding.',
+    },
+    {
+        icon: 'fa-solid fa-user-graduate',
+        title: 'Free Consultations',
+        description: 'Book a complimentary appointment and discuss your needs over a cup of coffee.',
+    },
+    {
+        icon: 'fa-solid fa-headset',
+        title: '24/7 Support',
+        description: 'Providing uninterrupted support to assist you anytime, every day.',
+    },
+])
 
  </script>
  
