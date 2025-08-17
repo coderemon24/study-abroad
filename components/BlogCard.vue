6650e9a92035a893c9843aa064383959044e353d
coderemon24 <mdemonahmed2021@gmail.com>
2025-08-17 15:54:08 +0600

    blog design done

---
+++ components/BlogCard.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+     <NuxtLink to="#" class="
+     bg-white
+     rounded-lg
+     shadow-md
+     overflow-hidden
+     block
+     ">
+       <img class="w-full h-70 object-cover" :src="image" alt="">
+       <div class="
+       p-5
+       pt-3
+       ">
+       
+          <div v-if="tags" class="
+          mb-3
+          flex
+          gap-2
+          ">
+            <span v-for="tag in tags" class="
+            text-[12px]
+            bg-blue-700/20
+            text-blue-700
+            px-4
+            py-0.5
+            rounded-full
+            hover:bg-blue-700
+            hover:text-white
+            cursor-pointer
+            transition-all
+            duration-300
+            linear
+            ">{{ tag }}</span>
+          </div>
+       
+         <h2 class="
+         text-xl
+         font-medium
+         mb-2
+         ">
+           {{ title }}
+         </h2>
+         <p class="
+         text-sm
+         text-gray-500
+         ">
+          {{ description }}</p>
+         
+         <div class="
+         flex
+         justify-between
+         mt-4
+         pb-0
+         text-gray-400
+         text-[12px]
+         ">
+           <p>
+            <i class="fa-regular fa-calendar mr-2"></i>
+            <span class="text-sm ">{{ date }}</span>
+           </p>
+           <NuxtLink :to="path" class="
+           text-sm
+           group
+           ">
+            Read More 
+            <i class="fa-solid text-sm group-hover:ml-2
+            transition-all
+            duration-300
+            linear
+            fa-chevron-right ml-1"></i>
+           </NuxtLink>
+         </div>
+         
+       </div>
+     </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import blogImg from '~/assets/images/slider/slide-1.jpeg'
+
+defineProps({
+  image: {
+    type: String,
+    default: blogImg
+  },
+  tags: {
+    type: Array,
+    default: 'USA'
+  },
+  title: {
+    type: String,
+    default: 'Study in Australia with scholarship'
+  },
+  description: {
+    type: String,
+    default: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...'
+  },
+  date: {
+    type: String,
+    default: '01 Jan 2023'
+  },
+  path: {
+    type: String,
+    default: '#'
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blogs.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="
+  text-black/70
+  pb-10
+  ">
+    <div class="container mx-auto w-11/12">
+        <!----- Breadcrumb ------->
+      <div class="breadcrumb pt-8 flex items-center gap-2 text-sm mb-6">
+        <NuxtLink class="text-blue-700 hover:underline" to="/">Home</NuxtLink>
+        <i class="fa-solid fa-chevron-right text-gray-500"></i>
+        <span class="text-gray-600">Blogs</span>
+      </div>
+    </div>
+    
+    <section class="container mx-auto w-11/12">
+       <div class="
+       my-10
+       grid
+       grid-cols-1
+       md:grid-cols-3
+       gap-10
+       ">
+          <div class="
+          grid
+          grid-cols-1
+          md:grid-cols-2
+          gap-8
+          md:col-span-2
+          ">
+          
+            <BlogCard v-for="item in blogs"
+            :image="item.image"
+            :title="item.title"
+            :description="item.description"
+            :tags="item.tags"
+            :date="item.date"
+            :path="item?.path"
+            />
+            
+          </div>
+          
+          <div class="
+          hidden
+          md:block
+          md:col-span-1
+          ">
+            <h2>
+              Latest Blogs
+            </h2>
+          </div>
+       </div>
+    </section>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+import blogImg from '~/assets/images/slider/slide-2.jpeg'
+import blogImg1 from '~/assets/images/slider/slide-1.jpeg'
+
+const blogs = [
+  {
+    image: blogImg,
+    title: 'Study in Australia with scholarship 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    tags: ['Canada','UK', 'USA'],
+    date: '03 July 2023',
+    path: '#'
+  },
+  
+  {
+    image: blogImg1,
+    title: 'Study in Australia with scholarship 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    tags: ['Canada','UK', 'USA'],
+    date: '03 July 2023',
+    path: '#'
+  }
+]
+
+</script>
+
+<style scoped>
+
+</style>
