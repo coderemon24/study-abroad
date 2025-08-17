108f10287e2250b1160df0ec427f64d54e77391f
coderemon24 <mdemonahmed2021@gmail.com>
2025-08-17 18:11:33 +0600

    events page done

---
+++ components/EventCard.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+     <NuxtLink to="#" class="
+     bg-white
+     rounded-lg
+     shadow-md
+     overflow-hidden
+     block
+     hover:-translate-y-2
+     transition-all
+     duration-300
+     ease-in-out
+     group
+     "
+     >
+       <div class="
+       overflow-hidden
+       ">
+        
+        <img 
+       class="w-full h-70 object-cover
+       group-hover:scale-105
+       transition-all
+       duration-300
+       ease-in-out
+       " :src="image" alt="">
+        
+       </div>
+       
+       <div 
+       class="
+       p-5
+       pt-3
+       ">
+       
+       
+         <h2 
+         class="
+         text-xl
+         font-medium
+         mb-2
+         ">
+           {{ title }}
+         </h2>
+         <p 
+         class="
+         text-sm
+         text-gray-500
+         ">
+          {{ description }}</p>
+         
+         <div 
+         class="
+         flex
+         justify-between
+         mt-4
+         pb-0
+         text-gray-400
+         text-[12px]
+         ">
+           <p>
+            <i class="fa-regular fa-clock mr-2"></i>
+            <span class="text-sm ">{{ date }}</span>
+           </p>
+           <NuxtLink :to="path" class="
+           text-sm
+           group
+           ">
+            Read More 
+            <i class="fa-solid text-sm group-hover:ml-2
+            transition-all
+            duration-300
+            linear
+            fa-chevron-right ml-1"></i>
+           </NuxtLink>
+         </div>
+         
+       </div>
+     </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import blogImg from '~/assets/images/slider/slide-1.jpeg'
+
+defineProps({
+  image: {
+    type: String,
+    default: blogImg
+  },
+  title: {
+    type: String,
+    default: 'Study in Australia with scholarship'
+  },
+  description: {
+    type: String,
+    default: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...'
+  },
+  date: {
+    type: String,
+    default: '01 Jan 2023, 10:00 AM'
+  },
+  path: {
+    type: String,
+    default: '#'
+  },
+})
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/events.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="
+  text-black/70
+  py-8
+  ">
+  
+   <section class="container mx-auto w-11/12">
+        <!----- Breadcrumb ------->
+      <div class="breadcrumb flex items-center gap-2 text-sm mb-6">
+        <NuxtLink class="text-blue-700 hover:underline" to="/">Home</NuxtLink>
+        <i class="fa-solid fa-chevron-right text-gray-500"></i>
+        <span class="text-gray-600">Events</span>
+      </div>
+    </section>
+  
+    <section class="container mx-auto w-11/12">
+      
+      <div class="mb-10 mt-10">
+            <h4 class="
+            text-gray-500
+            capitalize
+            mb-1
+            ">Events</h4>
+            <h2 class="
+            title-design
+            text-3xl
+            text-black/90
+            ">Discover Our Latest Seminars, Workshops & Student Meets</h2>
+          </div>
+          
+       <div class="
+       grid
+       grid-cols-1
+       md:grid-cols-3
+       gap-10
+       ">
+       
+        <EventCard v-for="(item, index) in events" 
+        :key="index" 
+        :image="item.image" 
+        :title="item.title" 
+        :description="item.description" 
+        :date="item.date" 
+        :path="item.path" />
+        
+       </div>
+    </section>
+    
+    <section class="container mx-auto w-11/12">
+      <div class="
+      mt-10
+      bg-white
+      p-5
+      md:p-10
+      rounded-lg
+      shadow
+      ">
+        <div class="
+          mb-3
+          md:w-[60%]
+          md:mx-auto
+          md:text-center
+          md:flex
+          md:flex-col
+          md:items-center
+          ">
+            <h1 class="
+            text-2xl
+            font-semibold
+            text-blue-800
+            md:text-center
+            md:text-4xl
+            ">
+               Join Our Community of Students.
+            </h1>
+            <p class="
+            text-gray-500
+            mt-3
+            text-justify
+            md:text-center
+            ">
+               Connect with like-minded learners, share experiences, and grow together in a supportive student community.
+            </p>
+            <NuxtLink class="
+            btn-outline
+            " to="#contact">
+               <span class="relative z-10">Join Now</span>
+            </NuxtLink>
+         </div>
+      </div>
+    </section>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { id } from '@nuxt/ui/runtime/locale/index.js';
+import eventImage from '~/assets/images/events/event1.jpg'
+
+const events = [
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+  {
+    image: eventImage,
+    title: 'Study in Australia with scholarship',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores voluptate Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    date: '01 Jan 2023, 10:00 AM',
+    path: '#'
+  },
+]
+
+</script>
+
+<style>
+
+</style>
